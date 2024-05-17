b0dbd4255093439506746f4ec4c54ada08bf13e0
polopo927 <polopo927@mail.ru>
2024-05-17 13:08:55 +0300

    пофиксил деплой

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { Card } from './components/Card'
 import { getList } from './components/request'
@@ -7,21 +7,11 @@ function App() {
 	const [dayList, setDayList] = useState([])
 
 	useEffect(() => {
-		getList('/public/data.json')
-		.then(data => {
-			setDayList(data.movielist)
-		})
-		.catch(e => console.log(e))
-	},[])
-	/* const dayList = [
-		{ day: 'Понедельник', film: 'Фильм1' },
-		{ day: 'Вторник', film: 'Фильм2' },
-		{ day: 'Среда', film: 'Фильм3' },
-		{ day: 'Четверг', film: 'Фильм4' },
-		{ day: 'Пятница', film: 'Фильм5' },
-		{ day: 'Суббота', film: 'Фильм6' },
-		{ day: 'Воскресенье', film: 'Фильм7' }
-	] */
+		getList('/data.json')
+			.then(data => setDayList(data.movielist))
+			.catch(e => console.log(e))
+	}, [])
+
 
 	return (
 		<div className='films'>
