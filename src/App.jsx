3086228782476343a64a5ce8cc2ea545fa680e43
polopo927 <polopo927@mail.ru>
2024-05-17 12:56:46 +0300

    добавил json и fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import { Card } from './components/Card'
+import { getList } from './components/request'
 
 function App() {
+	const [dayList, setDayList] = useState([])
 
-	const dayList = [
+	useEffect(() => {
+		getList('/public/data.json')
+		.then(data => {
+			setDayList(data.movielist)
+		})
+		.catch(e => console.log(e))
+	},[])
+	/* const dayList = [
 		{ day: 'Понедельник', film: 'Фильм1' },
 		{ day: 'Вторник', film: 'Фильм2' },
 		{ day: 'Среда', film: 'Фильм3' },
@@ -12,7 +21,7 @@ function App() {
 		{ day: 'Пятница', film: 'Фильм5' },
 		{ day: 'Суббота', film: 'Фильм6' },
 		{ day: 'Воскресенье', film: 'Фильм7' }
-	]
+	] */
 
 	return (
 		<div className='films'>
