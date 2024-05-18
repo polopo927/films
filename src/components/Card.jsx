93f78456c746233c9fe410652e80903780138511
polopo927 <polopo927@mail.ru>
2024-05-18 15:13:30 +0300

    плавный выезд нотес

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 export const Card = ({ place, film, date, notes }) => {
 
+	const arrowUp = '/up.svg'
+	const arrowDown = 'down.svg'
 	const [isNotesVisible, setIsNotesVisible] = useState(false)
 
 	const toggleNotes = () => {
@@ -11,17 +13,21 @@ export const Card = ({ place, film, date, notes }) => {
 	return (
 		<div className='card'>
 			<div>{place}
-			{notes && (
-				<div>
-					<button onClick={toggleNotes}>
-						{isNotesVisible ? '!' : '!'}
-					</button>
-					{isNotesVisible && <div className="notes">{notes}</div>}
-				</div>
-			)}
 			</div>
 			<a href="#">{film}</a>
 			<div>{date}</div>
+			{notes && (
+        <div>
+          <button onClick={toggleNotes}>
+            <img
+              src={isNotesVisible ? arrowUp : arrowDown}
+              alt={isNotesVisible ? "Стрелка вверх" : "Стрелка вниз"}
+              className="arrow"
+            />
+          </button>
+          <div className={`notes ${isNotesVisible ? 'notes-visible' : ''}`}>{notes}</div>
+        </div>
+      )}
 		</div>
 	)
 }
