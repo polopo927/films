5109bd680f052ff739db4f99a12063ff92500439
polopo927 <polopo927@mail.ru>
2024-05-20 12:41:08 +0300

    плавный нотес

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,32 +2,32 @@ import React, { useState } from "react";
 
 export const Card = ({ place, film, date, notes }) => {
 
-	const arrowUp = '/up.svg'
-	const arrowDown = 'down.svg'
+	const arrow = 'down.svg'
 	const [isNotesVisible, setIsNotesVisible] = useState(false)
 
 	const toggleNotes = () => {
 		setIsNotesVisible(!isNotesVisible)
+		console.log(isNotesVisible)
 	}
 
 	return (
 		<div className='card'>
-			<div>{place}
+			<div className="card__place">{place}
 			</div>
-			<a href="#">{film}</a>
-			<div>{date}</div>
+			<a className="card__film" href="#">{film}</a>
+			<div className="card__date">{date}</div>
 			{notes && (
-        <div>
-          <button onClick={toggleNotes}>
-            <img
-              src={isNotesVisible ? arrowUp : arrowDown}
-              alt={isNotesVisible ? "Стрелка вверх" : "Стрелка вниз"}
-              className="arrow"
-            />
-          </button>
-          <div className={`notes ${isNotesVisible ? 'notes-visible' : ''}`}>{notes}</div>
-        </div>
-      )}
+				<div className="card__dropdown">
+					<button onClick={toggleNotes} className="card__arrow">
+						<img
+							src={arrow}
+							alt="Открыть заметки"
+							className={`arrow ${isNotesVisible ? 'arrow-up' : ''}`}
+						/>
+					</button>
+					<div className="dropdownContent" data-open={isNotesVisible}>{notes}</div>
+				</div>
+			)}
 		</div>
 	)
 }
